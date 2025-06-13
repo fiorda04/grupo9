67af0e1dee9a4503651f4ccc64be4bf2acde927d
Daniel <31143269+DanyGeier@users.noreply.github.com>
2025-06-12 22:48:58 -0300

    TicketController adds
    Agrego cosas

--- src/main/java/com/oo2/grupo9/controllers/TicketRestController.java
+++ src/main/java/com/oo2/grupo9/controllers/TicketRestController.java
@@ -4,21 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oo2.grupo9.services.ITicketService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/api/tickets")
+@Tag(name = "3. Gestion de Tickets", description = "Endpoints para crear, buscar o eliminar tickets")
 public class TicketRestController {
 
 	@Autowired
 	private ITicketService ticketService;
 	
+	@PostMapping("/eliminar/{id}")
 	@PreAuthorize("hasRole('ROLE_Admin')")
-	public ResponseEntity<String> eliminarTicket(@PathVariable Long id){
+	public ResponseEntity<String> eliminarTicketPorId(@PathVariable Long id){
 		ticketService.eliminar(id);
 		return ResponseEntity.ok("Ticket eliminado correctamente");
 	}
--- src/main/java/com/oo2/grupo9/dtos/CreateTicketResponse.java
+++ src/main/java/com/oo2/grupo9/dtos/CreateTicketResponse.java
@@ -1,12 +1,18 @@
 package com.oo2.grupo9.dtos;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class CreateTicketResponse {
 
+	private Long id;
+	private String titulo;
+	private String descripcion;
+	private String tipo;
+	private String estado;
+	private String prioridad;
+	private List<Long> categoriaId;
+	private LocalDateTime fechaCreacion;
 	
 	
 }
